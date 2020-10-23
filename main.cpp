90cba2b7c1250c960d2508908f8170d48ca9057f
Calebe_Pereira <56983684+CalebePereira@users.noreply.github.com>
2020-10-23 17:06:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+int main(int argc, char *argv[]){
+    FILE *arq;
+    char *nomeUm; //arquivo entrada
+    char *nomeDois; //arquivo saida
+    char Linha[100];
+    char *result;
+    FILE *arqSaida;
+    
 
-int main(){
-    return 0;
+    nomeUm = argv[1];
+    nomeDois = argv[2];
+
+    printf("nome do arquivo: %s\n", nomeUm);
+    arq = fopen(nomeUm, "rt");
+    //arqSaida = fopen(nomeDois, "wt");
+    
+    if(arq == NULL){
+        printf("PROBLEMA\n");
+        return 0;
+    }
+
+    int i = 1;
+    while (!feof(arq))
+    {
+	    // Lê uma linha (inclusive com o '\n')
+        result = fgets(Linha, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
+        if (result)  // Se foi possível ler
+	    printf("Linha %d : %s\n",i,Linha);
+        i++;
+    }
+
+    //chamar o alg
+
+
+    //implementar a impressão no arquivo de saida RESULTADO
+
+     fclose(arq);
+     //fclose(arqSaida);
 }
