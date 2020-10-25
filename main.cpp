e39525d061c773adbf3e4a0b74afdc728f6a31bc
Calebe_Pereira <56983684+CalebePereira@users.noreply.github.com>
2020-10-25 12:05:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,14 +14,19 @@ void imprime_fila(queue<int> fila){
 
 void espancao(char *info){
     int i=0;
+    int j=0;
+    int obstaculo = info[6]; //ARMAZENA QUANTIDADE DE OBSTACULOS info[6] 
+    bool achou = false;
+    queue<int> fila; //CRIA A FILA, PARA ENVIAR A FILA É SÓ O NOME.
+    int grid[info[0]][info[1]]; //info[0] representa o n de linhas e info[1] representa o n de colunas
 
-    while(info[i] != '\0'){
-        if(info[i] == ' '){
-            i++;
-        }
-        printf("TESTE VET: %c\n", info[i]);
-        i++;
-    }
+    grid[info[2]][info[3]] = 0; //inicializa o indice da origem com 0
+
+    //insere a origem na fila
+    fila.push(grid[info[2]][info[3]]);
+
+    //A PARTIR DAQUI IMPLEMENTAR O WHILE DA ESPANCAO
+
 }
 
 
@@ -32,9 +37,9 @@ int main(int argc, char *argv[]){
     char Linha[100];
     char *result;
     FILE *arqSaida;
-    queue<int> fila; //CRIA A FILA, PARA ENVIAR A FILA É SÓ O NOME.
+    //queue<int> fila; //CRIA A FILA, PARA ENVIAR A FILA É SÓ O NOME.
 
-   int vetInfo[100]; //vetor com os dados do arquivo
+   int vetInfo[10000]; //vetor com os dados do arquivo 
 
     
 
@@ -63,6 +68,7 @@ int main(int argc, char *argv[]){
     }
     //0BS: chamar o alg
     //espancao(vetInfo);
+    
     //implementar a impressão no arquivo de saida RESULTADO
 
      fclose(arq);
