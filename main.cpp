30931982ac67735790b68148ee4813cee015e031
rafavini <42884321+rafavini@users.noreply.github.com>
2020-10-27 12:35:58 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,19 @@
 #include <iostream>
 #include <queue>
 #define MAX 1000
+#define INT_MAX 2147483647
 
 using namespace std;
 
 //funçao que imprime a fila.
 //METODOS FILA.SIZE, FILA.PUSH, FILA.POP, FILA.FRONT, FILA.BACK
 void imprime_fila(queue<int> fila){
-    for(int i = fila.front(); i < fila.size();i++){
-        printf("%d",i);
+    while(!fila.empty()){
+        printf("%d",fila.front());
+        fila.pop();
     }
 }
 
-/*void linhas_e_col(int mat[][], int a, int b, int pos_l, int pos_c){
-    for(int i=pos_l;i<a;i++){//preebche as linhas e colunas que possuem obstaculo com -1
-        for(int j=pos_c;j<b;j++){
-            mat[i][j]=-1;
-        }
-    }
-}
-*/
-
-/*void expansao(int *info){
-    int a=7;
-    int obstaculo = info[6]; //ARMAZENA QUANTIDADE DE OBSTACULOS info[6] 
-    bool achou = false;
-    queue<int> fila; //CRIA A FILA, PARA ENVIAR A FILA É SÓ O NOME.
-    int grid[info[0]][info[1]]; //info[0] representa o n de linhas e info[1] representa o n de colunas
-
-    int aux = 0;
-    //inicializar as celulas do grid que possuem obstaculo com -1 
-    while(aux < obstaculo){//executa enquanto for menos que a quantidade de obstaculos
-
-        grid[info[a]][info[a+1]] = -1; //pega o indice do inicio do primeiro obstaculo e inicializa com -1
-        a++;
-        linhas_e_col(grid, info[a+1], info[a+2], a-1, a);//chama a funcao para preencher o numero de linhas e colunas com obstaculo a partir do indice de origem
-        a=a+3;//pega proxima posicao com informacoes de outro obstaculo
-
-        aux++;
-        
-    }
-
-    grid[info[2]][info[3]] = 0; //inicializa o indice da origem com 0
-
-    //INICIALIZAR POSICOES DIFERENTE DE O E -1 COM INFINITO
-
-    //insere a origem na fila
-    fila.push(grid[info[2]][info[3]]);
-
-    //A PARTIR DAQUI IMPLEMENTAR O WHILE DA ESPANCAO
-
-}
-*/
 
 int main(int argc, char *argv[]){
     FILE *arq;
@@ -63,6 +25,8 @@ int main(int argc, char *argv[]){
     FILE *arqSaida;
     int aux2=0;
     queue<int> fila; //CRIA A FILA, PARA ENVIAR A FILA É SÓ O NOME.
+    bool achou = false;
+    
 
    int vetInfo[100]; //vetor com os dados do arquivo
    
@@ -89,12 +53,12 @@ int main(int argc, char *argv[]){
     }
 
     int grid[vetInfo[0]][vetInfo[1]]; // criar o grid com o tamanho de pos 0 e pos 1 no vetor;
+    grid[vetInfo[2]][vetInfo[3]] = 0; //COLOCANDO 0 NA ORIGEM
 
-
-    //ZERANDO MATRIZ 
+    //COLOCANDO O MAXIMO INTEIRO POSSIVEL NA MATRIZ 
      for(int t=0; t < vetInfo[0];t++){
         for(int v =0; v < vetInfo[1];v++){
-            grid[t][v] = 0;
+            grid[t][v] = INT_MAX;
         }
     }
     
@@ -111,8 +75,12 @@ int main(int argc, char *argv[]){
     pos_lin = 9;
     col = vetInfo[10];
     pos_col = 10;
+
+    
     
 
+
+    // COLOCANDO OS OBSTACULOS NA MATRIZ
     while(aux < obstaculo){
         grid[i][j] = -1; //INICIA O I,J DO PRIMEIRO OBSTACULO COM -1
 
@@ -155,17 +123,26 @@ int main(int argc, char *argv[]){
         col = vetInfo[pos_col];
      
     }
+    //IMPRIMINDO A MATRIZ
     printf("\n");
-   for(int t=0; t < vetInfo[0];t++){
+   /*for(int t=0; t < vetInfo[0];t++){
         for(int v =0; v < vetInfo[1];v++){
             printf("%d \t",grid[t][v]);
         }
         printf("\n");
     }
+    */
+
+    
     
 
     //0BS: chamar o alg
-    //espancao(vetInfo);
+    printf("teste\n");
+   fila.push(vetInfo[2]);
+   fila.push(vetInfo[3]);
+    imprime_fila(fila);
+
+    
     //implementar a impressão no arquivo de saida RESULTADO
 
      fclose(arq);
